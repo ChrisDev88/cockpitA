d7b42130f11e95f8a01e2044a0a119cc76ae3fe8
ChrisDev88 <c.fuss@datatrain.de>
2019-07-11 22:03:41 +0200

    update

--- app/src/main/java/de/datatrain/cockpita/app/cockpit/modules/mk/TenantSearch.kt
+++ app/src/main/java/de/datatrain/cockpita/app/cockpit/modules/mk/TenantSearch.kt
@@ -1,11 +1,25 @@
 package de.datatrain.cockpita.app.cockpit.modules.mk
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.design.widget.Snackbar
+import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import de.datatrain.cockpita.R
 import de.datatrain.cockpita.app.cockpit.BaseActivity
+import kotlinx.android.synthetic.main.activity_tenant_search.*
 import kotlinx.android.synthetic.main.activity_tenant_search2.*
+import kotlinx.android.synthetic.main.activity_tenant_search2.toolbar
+import java.util.*
 
 class TenantSearch : BaseActivity() {
 
@@ -13,5 +27,21 @@ class TenantSearch : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tenant_search)
         setSupportActionBar(toolbar)
+
+        val inputDate : EditText = findViewById(de.datatrain.cockpita.R.id.inputRecnDate)
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        inputDate.setOnClickListener{
+            val dpd : DatePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view: DatePicker, i: Int, i1: Int, i2: Int ->
+                inputDate.setText(view.getYear().toString() + "." + (view.getMonth()+1) + "." + view.getDayOfMonth())
+            },year,month,day)
+            dpd.show()
+        }
     }
 }
+
+
+
